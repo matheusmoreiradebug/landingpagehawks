f1d0b09febc8e910b71551b5b0b9301e5186a163
matheusmoreiradebug <matheus189moreira@gmail.com>
2026-05-05 11:52:45 -0300

    fix: corrigindo erro no cron

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
+  root: ".",
   build: {
     outDir: "dist",
-    assetsDir: "assets",
+    rollupOptions: {
+      input: resolve(__dirname, "index.html"),
+    },
   },
 });
